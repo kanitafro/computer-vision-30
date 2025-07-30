08f8b211d5680a513bfc938d2eaf22e41d6973b4
Kanita Tafro <tafro.kanita@gmail.com>
2025-07-30 08:50:08 +0200

    Update helper_functions1.py

--- Project1-OpenCV/helper_functions1.py
+++ Project1-OpenCV/helper_functions1.py
@@ -34,3 +34,48 @@ def show_coordinates(event, x, y, flags, param):
         
         # Update the window
         cv2.imshow(window_name, img_with_coords)
+
+def setup_mouse_callback(window_name, image):
+    """
+    Set up mouse callback to display coordinates and RGB values in a white footer below the image.
+    
+    Args:
+        window_name (str): Name of the OpenCV window
+        image (numpy.ndarray): The image to display (in BGR format)
+    """
+    # Create white footer area (50 pixels tall)
+    footer_height = 50
+    footer = np.full((footer_height, image.shape[1], 3), 255, dtype=np.uint8)  # White background
+    
+    # Combine image and footer
+    display_image = np.vstack((image, footer))
+    
+    def mouse_callback(event, x, y, flags, param):
+        nonlocal display_image
+        
+        # Clear the footer (set to white)
+        footer.fill(255)
+        
+        if 0 <= x < image.shape[1] and 0 <= y < image.shape[0]:
+            # Get BGR values (OpenCV uses BGR format)
+            b, g, r = image[y, x]
+            
+            # Create text for coordinates and BGR values
+            coord_text = f'X: {x}, Y: {y}'
+            bgr_text = f'B: {b}, G: {g}, R: {r}'
+            
+            # Put black text in the footer
+            cv2.putText(footer, coord_text, (10, 20), 
+                       cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 0, 0), 2)  # Black text
+            cv2.putText(footer, bgr_text, (10, 45), 
+                       cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 0, 0), 2)  # Black text
+        
+        # Recombine image and footer
+        display_image = np.vstack((image, footer))
+        cv2.imshow(window_name, display_image)
+    
+    # Set the mouse callback function
+    cv2.setMouseCallback(window_name, mouse_callback)
+    
+    # Display the initial image
+    cv2.imshow(window_name, display_image)
